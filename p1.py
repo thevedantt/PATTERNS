270ecea402a8a5324d5d871faf2aacbb24f4c096
thevedantt <157893956+thevedantt@users.noreply.github.com>
2025-06-20 23:43:38 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,17 @@
+# print("*")
+
+# n=5
+# print("*"*n)
+
+# for j in range (n):
+#     print('*')
+
+# for j in range (n):
+#     print('*', end = '')
+
+n=5
+for j in range (n):
+    for i in range (n):
+        print('*',end=' ')
+    print()
+    
